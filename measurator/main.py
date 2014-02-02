d7b361009c10a7f3f33b4a39da41a21ff0ed2349
Andrey Hitrin <andrey.hitrin@gmail.com>
2014-02-02 19:13:44 +0600

    Count statistics

--- measurator/main.py
+++ measurator/main.py
@@ -1,7 +1,26 @@
-import argparse
+import argparse, csv
 
 def run_main():
+    fails = 0
+    succeeds = 0
+    not_yet = 0
     path = file_path()
+    with open(path) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            status = row[0]
+            if status == 'F':
+                fails = fails + 1
+            elif status == 'S':
+                succeeds = succeeds + 1
+            else:
+                not_yet = not_yet + 1
+    total_done = fails + succeeds
+    if total_done > 0:
+        percentage = '%d%%' % (float(100 * succeeds) / float(total_done))
+    else:
+        percentage = 'N/A'
+    print "Succesful predictions:", percentage, ", not done yet:", not_yet
 
 def file_path():
     parser = argparse.ArgumentParser()
