5c681567c359c76e9e323a82ab9162f5098b6421
Andrey Hitrin <andrey.hitrin@gmail.com>
2014-02-02 18:49:50 +0600

    Add mandatory argument: path to file

--- measurator/main.py
+++ measurator/main.py
@@ -1,2 +1,10 @@
+import argparse
+
 def run_main():
-    pass
+    path = file_path()
+
+def file_path():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    args = parser.parse_args()
+    return args.path
