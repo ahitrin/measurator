91588a8a851c59d3a65e44a161fe0190d63b5c00
Andrey Hitrin <andrey.hitrin@gmail.com>
2014-02-02 18:34:08 +0600

    Simplest application skeleton

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='measurator',
+    version=0.1,
+    url='https://ahitrin.github.io',
+    license='MIT',
+    author='Andrey Hitrin',
+    author_email='andrey.hitrin@gmail.com',
+    description='Simple command-line app aimed to record and evaluate measures',
+    packages=['measurator'],
+    platform='any',
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 1 - Alpha',
+        ],
+)
