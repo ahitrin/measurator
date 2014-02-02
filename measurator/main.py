fe99dff3b7ada335272821116577b223831509e6
Andrey Hitrin <andrey.hitrin@gmail.com>
2014-02-02 19:39:11 +0600

    Undone: detect which predictions need to be evaluated

--- measurator/main.py
+++ measurator/main.py
@@ -1,10 +1,11 @@
-import argparse, csv
+import argparse, csv, datetime, time
 
 def run_main():
     fails = 0
     succeeds = 0
-    not_yet = 0
+    not_yet = list()
     path = file_path()
+    # read file
     with open(path) as f:
         reader = csv.reader(f)
         for row in reader:
@@ -14,13 +15,20 @@ def run_main():
             elif status == 'S':
                 succeeds = succeeds + 1
             else:
-                not_yet = not_yet + 1
+                not_yet.append(row)
+    # evaluate measurements
+    now = datetime.datetime.now()
+    for row in not_yet:
+        evaluate_time = datetime.datetime(*(time.strptime(row[1], '%Y-%m-%d %H:%M:%S')[:6]))
+        if evaluate_time < now:
+            print "Time to evaluate:", row[2]
+    # print total statistics
     total_done = fails + succeeds
     if total_done > 0:
         percentage = '%d%%' % (float(100 * succeeds) / float(total_done))
     else:
         percentage = 'N/A'
-    print "Succesful predictions:", percentage, ", not done yet:", not_yet
+    print "Succesful predictions:", percentage, ", not done yet:", len(not_yet)
 
 def file_path():
     parser = argparse.ArgumentParser()
