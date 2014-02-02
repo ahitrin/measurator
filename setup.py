b9731f582828598564af5d8931571fe95065b9d7
Andrey Hitrin <andrey.hitrin@gmail.com>
2014-02-03 00:53:26 +0600

    Very version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='measurator',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/ahitrin/measurator-proto',
     license='MIT',
     author='Andrey Hitrin',
