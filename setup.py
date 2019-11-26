449eb1bed09677c22fa4ab0b8579735ff507ffbd
Andrey Hitrin <andrey.hitrin@gmail.com>
2019-11-26 11:27:32 +0500

    Version 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='measurator',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/ahitrin-attic/measurator-proto',
     license='MIT',
     author='Andrey Hitrin',
