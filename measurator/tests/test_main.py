a5d3aa9bdf642c1a2b0067026314da949d76ef2d
Andrey Hitrin <andrey.hitrin@gmail.com>
2020-01-31 13:33:20 +0500

    More tests

--- measurator/tests/test_main.py
+++ measurator/tests/test_main.py
@@ -4,12 +4,12 @@ from typing import List
 from approvaltests import verify
 from approvaltests.reporters import GenericDiffReporterFactory
 
-from measurator import run_main_
+from measurator import run_main_, TIME_FORMAT
 from measurator.domain import IO
 
 
 class TestableIO(IO):
-    def __init__(self, inputs: List[str], timestamps: List[datetime.datetime], file_content: iter):
+    def __init__(self, inputs: List[str], timestamps: List[str], file_content: iter):
         self.file_content = file_content
         self.inputs = inputs
         self.timestamps = timestamps
@@ -19,7 +19,7 @@ class TestableIO(IO):
         self.log.append(f" {text}{''.join(str(x) for x in args)}")
 
     def read(self) -> str:
-        output = self.inputs.pop()
+        output = self.inputs.pop(0)
         self.log.append(f"> {output}")
         return output
 
@@ -31,14 +31,24 @@ class TestableIO(IO):
         return self.file_content
 
     def now(self) -> datetime.datetime:
-        timestamp = self.timestamps.pop()
+        timestamp = self.timestamps.pop(0)
         self.log.append(f"@ {timestamp}")
-        return timestamp
+        return datetime.datetime.strptime(timestamp, TIME_FORMAT)
 
 
-def test_approve():
+def test_empty_file_no_predictions():
     inputs = ['N']
     timestamps = []
+    file_content = []
+    io = TestableIO(inputs, timestamps, file_content)
+    run_main_(io)
+    reporter = GenericDiffReporterFactory().get_first_working()
+    verify("\n".join(io.log), reporter)
+
+
+def test_empty_file_add_prediction():
+    inputs = ['Y', 'Prediction 1', '2020-02-01 09:00']
+    timestamps = ['2020-01-31 12:00']
     file_content = []
     io = TestableIO(inputs, timestamps, file_content)
     run_main_(io)
